b9f204d9ad98b64a455c556f5b021b639f4fb4fb
Mohamed Fathy <codetwostart@gmail.com>
2019-11-09 14:21:19 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace methodparams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 2, 4, 6, 9 };
+            //PrintArray("Mohamed");
+            //Foo("Mohamed", 20, 'C', true);
+            Foo();
+        }
+        static void PrintArray(string x, params int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"items = {array[i]}");
+            }
+        }
+        static void Foo(params object[] items)
+        {
+            foreach (object item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
